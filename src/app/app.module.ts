6aa03c8d6cbea2aacbe5d317dbc930ef525cc190
fcox <isc-fco@outlook.com>
2019-01-08 03:04:31 -0600

    Firechat listo

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { environment } from '../environments/environment';
+
+import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+
+// Servicios
+import { ChatService } from './providers/chat.service';
+import { LoginComponent } from './components/login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule
+  ],
+  providers: [
+    ChatService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAPw_WQr7X4fEJz1mxl5INT6VgnOnTTiBY',
+    authDomain: 'firechat-e01d4.firebaseapp.com',
+    databaseURL: 'https://firechat-e01d4.firebaseio.com',
+    projectId: 'firechat-e01d4',
+    storageBucket: 'firechat-e01d4.appspot.com',
+    messagingSenderId: '678383139194'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
